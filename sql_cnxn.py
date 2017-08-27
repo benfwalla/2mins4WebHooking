ac4d05694eed65b9a816e97d38d0b2bea5f494bc
benfwalla <benfwalla@gmail.com>
2017-08-27 14:31:52 -0400

    Cleaning

--- sql_cnxn.py
+++ sql_cnxn.py
@@ -2,16 +2,6 @@ import mysql.connector
 
 # https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-transaction.html
 
-try:
-	cnxn = mysql.connector.connect(user = '',
-								   password = '',
-								   host = '',
-								   database = '')
-except:
-	print("Error: Unable to connect to MySQL Server.")
-
-cursor = cnxn.cursor()
-
 # Creates a new row in a MySQL table
 # Input type: tuple of strings (i.e. ('PIT', '4', 'NYR', '2'))
 def create_game_row(game_data):
